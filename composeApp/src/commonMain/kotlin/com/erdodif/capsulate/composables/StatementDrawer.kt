c986657fcba729d5d7005556784a19051537b333
erdodif <erdodif@gmail.com>
2024-10-08 10:18:58 +0200

    Minimap DND source animation + disable scroll of statementDrawer

--- composeApp/src/commonMain/kotlin/com/erdodif/capsulate/composables/StatementDrawer.kt
+++ composeApp/src/commonMain/kotlin/com/erdodif/capsulate/composables/StatementDrawer.kt
@@ -1,5 +1,13 @@
 package com.erdodif.capsulate.composables
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Animation
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateSizeAsState
+import androidx.compose.animation.expandIn
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -9,8 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.erdodif.capsulate.globalDragState
 import com.erdodif.capsulate.lang.grammar.Expression
@@ -49,24 +60,36 @@ fun StatementDrawer(modifier: Modifier = Modifier) {
             )
         )
     }
-    LazyColumn(modifier.padding(25.dp, 10.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    LazyColumn(
+        modifier.padding(25.dp, 10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        userScrollEnabled = globalDragState.current.draggedItem == null
+    ) {
         items(statements) { statement ->
             val state = globalDragState.current
+            var animate by remember(state) { mutableStateOf(false) }
             DraggableItem(Modifier, key = statement, state = state,
                 data = Expression(
                     StrLit("TODO", MatchPos(0, 0))
                 ),
                 dragAfterLongPress = true,
                 draggableContent = {
+                    AnimatedVisibility(
+                        animate,
+                        enter = expandIn(),
+                    ) {
+                        statement.show(
+                            Modifier.border(Theme.borderWidth, Theme.borderColor).fillMaxWidth()
+                        )
+                    }
+                    animate = true
+                },
+                content = {
                     statement.show(
-                        Modifier.border(Theme.borderWidth, Theme.borderColor).fillMaxWidth()
+                        Modifier.padding(0.dp, 10.dp).border(Theme.borderWidth, Theme.borderColor)
+                            .fillMaxWidth()
                     )
-                }) {
-                statement.show(
-                    Modifier.padding(0.dp, 10.dp).border(Theme.borderWidth, Theme.borderColor)
-                        .fillMaxWidth()
-                )
-            }
+                })
         }
     }
 }
