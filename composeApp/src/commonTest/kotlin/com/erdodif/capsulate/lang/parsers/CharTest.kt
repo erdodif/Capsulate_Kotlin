ea22d50cd00888c18dbaf448e0e5ec29026d8cb0
erdodif <erdodif@gmail.com>
2024-11-24 00:04:56 +0100

    Unify character tests

--- composeApp/src/commonTest/kotlin/com/erdodif/capsulate/lang/parsers/CharTest.kt
+++ composeApp/src/commonTest/kotlin/com/erdodif/capsulate/lang/parsers/CharTest.kt
@@ -1,73 +1,74 @@
 package com.erdodif.capsulate.lang.parsers
 
-import com.erdodif.capsulate.assertFail
-import com.erdodif.capsulate.assertPass
-import com.erdodif.capsulate.assertPassAt
-import com.erdodif.capsulate.assertValue
-import com.erdodif.capsulate.lang.util.MatchPos
-import com.erdodif.capsulate.lang.util.ParserState
+import com.erdodif.capsulate.at
+import com.erdodif.capsulate.fail
 import com.erdodif.capsulate.lang.program.grammar.char
 import com.erdodif.capsulate.lang.program.grammar.digit
 import com.erdodif.capsulate.lang.program.grammar.satisfy
+import com.erdodif.capsulate.lang.util.MatchPos
+import com.erdodif.capsulate.pass
+import com.erdodif.capsulate.withValue
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class CharTest {
     @Test
-    fun char_match_single_character() = assertPassAt(
-        ParserState("c").parse(char('c')), MatchPos(0, 1)
-    )
-
-    @Test
-    fun char_returns_single_character() = assertValue(
-        'c',
-        ParserState("c").parse(char('c'))
-    )
-
-    @Test
-    fun char_fails_empty_string_run() = assertFail(ParserState("").run { char('c')() })
-
-    @Test
-    fun char_fails_empty_string_parse() = assertFail(ParserState("").parse(char('c')))
-
-    @Test
-    fun digit_pass() {
-        assertValue(0, ParserState("0").parse(digit))
-        assertValue(1, ParserState("1").parse(digit))
-        assertValue(2, ParserState("2").parse(digit))
-        assertValue(3, ParserState("3").parse(digit))
-        assertValue(4, ParserState("4").parse(digit))
-        assertValue(5, ParserState("5").parse(digit))
-        assertValue(6, ParserState("6").parse(digit))
-        assertValue(7, ParserState("7").parse(digit))
-        assertValue(8, ParserState("8").parse(digit))
-        assertValue(9, ParserState("9").parse(digit))
+    fun `char matches a single character`() {
+        char('c') pass "c" at MatchPos(0,1)
     }
 
     @Test
-    fun digit_fail() {
-        assertFail(ParserState("a").parse(digit))
-        assertFail(ParserState("_").parse(digit))
-        assertFail(ParserState("").parse(digit))
+    fun `char returns a single character`() {
+        char('c') pass "c" withValue 'c'
     }
 
     @Test
-    fun satisfy_pass_true() {
-        assertPass(ParserState("c").parse(satisfy { true }))
-        assertPass(ParserState(".").parse(satisfy { true }))
+    fun `char fails empty string`() {
+        char('c') fail ""
     }
 
     @Test
-    fun satisfy_fail_true_empty() =
-        assertFail(ParserState("").parse(satisfy { true }))
+    fun `digit passes with correct value`() {
+        digit pass "0" withValue 0
+        digit pass "1" withValue 1
+        digit pass "2" withValue 2
+        digit pass "3" withValue 3
+        digit pass "4" withValue 4
+        digit pass "5" withValue 5
+        digit pass "6" withValue 6
+        digit pass "7" withValue 7
+        digit pass "8" withValue 8
+        digit pass "9" withValue 9
+    }
 
     @Test
-    fun satisfy_fail_false() =
-        assertFail(ParserState("c").parse(satisfy { false }))
+    fun `digit fails`() {
+        digit fail "a" at 1
+        digit fail "_" at 1
+        digit fail "" at 0
+    }
 
     @Test
-    fun satisfy_pass_func() = assertPass(ParserState("c").parse(satisfy {
-        assertEquals('c', it)
-        true
-    }))
+    fun `satisfy passes constant true`() {
+        satisfy { true } pass "c"
+        satisfy { true } pass "."
+    }
+
+    @Test
+    fun `satisfy fails constant true on empty`(){
+        satisfy { true } fail ""
+    }
+
+    @Test
+    fun `satisfy fails constant false`() {
+        satisfy { false } fail ""
+    }
+
+    @Test
+    fun `satisfy passes value into function call correctly`() {
+        satisfy {
+            assertEquals('c', it)
+            true
+        } pass "c"
+    }
 }
