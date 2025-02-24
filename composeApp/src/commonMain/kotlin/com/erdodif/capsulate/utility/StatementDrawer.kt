d9953f678a2c65719febb5af53ce6e5abba2f080
erdodif <erdodif@gmail.com>
2025-02-25 00:15:04 +0100

    Polish Statement Drawer

--- composeApp/src/commonMain/kotlin/com/erdodif/capsulate/utility/StatementDrawer.kt
+++ composeApp/src/commonMain/kotlin/com/erdodif/capsulate/utility/StatementDrawer.kt
@@ -3,59 +3,116 @@
 package com.erdodif.capsulate.utility
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material.icons.rounded.KeyboardArrowDown
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
+import androidx.compose.material.icons.rounded.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
-import com.erdodif.capsulate.lang.program.grammar.Assign
-import com.erdodif.capsulate.lang.program.grammar.Atomic
-import com.erdodif.capsulate.lang.program.grammar.expression.BoolLit
-import com.erdodif.capsulate.lang.program.grammar.DoWhile
-import com.erdodif.capsulate.lang.program.grammar.If
-import com.erdodif.capsulate.lang.program.grammar.expression.IntLit
-import com.erdodif.capsulate.lang.program.grammar.Parallel
-import com.erdodif.capsulate.lang.program.grammar.Skip
-import com.erdodif.capsulate.lang.program.grammar.Wait
-import com.erdodif.capsulate.lang.program.grammar.When
-import com.erdodif.capsulate.lang.program.grammar.While
-import com.erdodif.capsulate.lang.util.MatchPos
 import com.erdodif.capsulate.structogram.composables.Theme
-import com.erdodif.capsulate.structogram.statements.AtomicStatement
-import com.erdodif.capsulate.structogram.statements.AwaitStatement
-import com.erdodif.capsulate.structogram.statements.Block
-import com.erdodif.capsulate.structogram.statements.Command
 import com.erdodif.capsulate.structogram.statements.DropStatement
-import com.erdodif.capsulate.structogram.statements.IfStatement
-import com.erdodif.capsulate.structogram.statements.LoopStatement
-import com.erdodif.capsulate.structogram.statements.ParallelStatement
-import com.erdodif.capsulate.structogram.statements.WhenStatement
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
-
-private val statements = listOf(
-    DropStatement("skip\n"),
-    DropStatement("if true {\n  skip\n} else {\n  skip\n}\n"),
-    DropStatement("when{\n  case1: skip,\n  case2: skip,\n  else: skip\n}\n"),
-    DropStatement("while true {\n  skip\n}\n"),
-    DropStatement("do {\n  skip\n} while true\n"),
-    DropStatement("await condition {\n  skip\n}\n"),
-    DropStatement("{ skip }|{ skip }"),
-)
 
 @OptIn(ExperimentalUuidApi::class)
 @Composable
 fun StatementDrawer(modifier: Modifier = Modifier) {
+    val statementModifier =
+        Modifier.padding(0.dp, 10.dp).border(Theme.borderWidth, Theme.borderColor)
+    var whenCount by remember { mutableStateOf(2) }
+    var parallelCount by remember { mutableStateOf(2) }
     LazyColumn(
         modifier.padding(25.dp, 10.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        items(statements) { statement ->
-            statement.Show(
-                Modifier.padding(0.dp, 10.dp).border(Theme.borderWidth, Theme.borderColor), true
-            )
+        item { DropStatement("skip\n").Show(statementModifier) }
+        item { DropStatement("if true {\n  skip\n} else {\n  skip\n}\n").Show(statementModifier) }
+        item {
+            Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                Column {
+                    IconButton({ whenCount = min(whenCount + 1, 5) }) {
+                        Icon(
+                            Icons.Rounded.KeyboardArrowUp, "",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                    IconButton({ whenCount = max(whenCount - 1, 1) }) {
+                        Icon(
+                            Icons.Rounded.KeyboardArrowDown, "",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
+                key(whenCount) {
+                    DropStatement(
+                        buildString {
+                            append("when{\n")
+                            for (i in 0..whenCount) {
+                                append("  case${i + 1}: skip,\n")
+                            }
+                            append("\n}\n")
+                        }).Show(statementModifier)
+                }
+
+            }
+
+        }
+        item { DropStatement("while true {\n  skip\n}\n").Show(statementModifier) }
+        item { DropStatement("do {\n  skip\n} while true\n").Show(statementModifier) }
+        item { DropStatement("await condition {\n  skip\n}\n").Show(statementModifier) }
+        item {
+            Column {
+                DropStatement(
+                    buildString {
+                        for (i in 0..parallelCount) {
+                            append("{ skip }")
+                            append("|")
+                        }
+                    }.dropLast(1)
+                ).Show(statementModifier)
+                Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                    IconButton({ parallelCount = max(parallelCount - 1, 1) }) {
+                        Icon(
+                            Icons.AutoMirrored.Filled.KeyboardArrowLeft, "",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                    IconButton({ parallelCount = min(parallelCount + 1, 5) }) {
+                        Icon(
+                            Icons.AutoMirrored.Filled.KeyboardArrowRight, "",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
+            }
         }
     }
 }
