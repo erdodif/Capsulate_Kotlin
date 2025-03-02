f40e243d1036a3841f5a166985827bea772994dc
erdodif <erdodif@gmail.com>
2025-03-03 00:20:54 +0100

    fix assign test

---
+++ composeApp/src/commonTest/kotlin/com/erdodif/capsulate/lang/evaluation/statements/SkipTest.kt
@@ -0,0 +1,37 @@
+package com.erdodif.capsulate.lang.evaluation.statements
+
+import com.erdodif.capsulate.lang.evaluation.MockEnvironments.Companion.emptyEnv
+import com.erdodif.capsulate.lang.evaluation.MockEnvironments.Companion.intEnv
+import com.erdodif.capsulate.lang.program.evaluation.Finished
+import com.erdodif.capsulate.lang.program.grammar.Skip
+import com.erdodif.capsulate.lang.program.grammar.expression.VNat
+import com.erdodif.capsulate.lang.util.MatchPos
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+
+class SkipTest {
+
+    @Test
+    fun `skip on empty environment`() {
+        val env = emptyEnv()
+        val result = Skip(MatchPos.ZERO).evaluate(env)
+        assertIs<Finished>(result)
+        assertEquals(0, env.parameters.size)
+        assertEquals(0, env.functions.size)
+        assertEquals(0, env.methods.size)
+    }
+
+    @Test
+    fun `skip on non-empty environment`() {
+        val env = intEnv()
+        val result = Skip(MatchPos.ZERO).evaluate(env)
+        assertIs<Finished>(result)
+        assertEquals(3, env.parameters.size)
+        assertEquals(0, env.functions.size)
+        assertEquals(0, env.methods.size)
+        assertEquals(1, (env.parameters[0].value as VNat).value)
+        assertEquals(4, (env.parameters[1].value as VNat).value)
+        assertEquals(7, (env.parameters[2].value as VNat).value)
+    }
+}
