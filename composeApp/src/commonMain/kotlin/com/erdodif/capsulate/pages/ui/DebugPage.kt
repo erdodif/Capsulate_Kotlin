da0f63c6644f09682d704a4111b266dbef33586b
erdodif <erdodif@gmail.com>
2025-03-22 01:51:19 +0100

    unused import

--- composeApp/src/commonMain/kotlin/com/erdodif/capsulate/pages/ui/DebugPage.kt
+++ composeApp/src/commonMain/kotlin/com/erdodif/capsulate/pages/ui/DebugPage.kt
@@ -29,10 +29,8 @@ import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
