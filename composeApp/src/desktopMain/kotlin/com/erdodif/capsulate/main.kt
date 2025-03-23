44f8a3a19b03565f20a32e031375775477122e44
erdodif <erdodif@gmail.com>
2025-03-23 01:11:45 +0100

    Add icons

--- composeApp/src/desktopMain/kotlin/com/erdodif/capsulate/main.kt
+++ composeApp/src/desktopMain/kotlin/com/erdodif/capsulate/main.kt
@@ -5,7 +5,9 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.erdodif.capsulate.resources.Res
 import com.erdodif.capsulate.resources.app_name
+import com.erdodif.capsulate.resources.ic_logo_foreground_paddingless
 import kotlinx.coroutines.coroutineScope
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import java.awt.Dimension
 
@@ -20,8 +22,9 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = appName,
+        icon = painterResource(Res.drawable.ic_logo_foreground_paddingless)
     ) {
-        window.minimumSize = Dimension(450,400)
+        window.minimumSize = Dimension(460,400)
         App()
     }
 }
